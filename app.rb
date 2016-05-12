946a0cc4b44b6d4c16c6cb1ef6a5418803a54cc7
Viktor <fullzp@mail.ru>
2016-05-13 00:09:53 +0300

    add_repository

---
+++ app.rb
@@ -0,0 +1,67 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+   	@email=params[:email]
+	@mess=params[:mess]
+	@message="Thank you, your qweshon is saved"
+	f = File.open "./public/email.txt", "a"
+	f.write "Email:\n#{@email}\nMessage:\n#{@mess}\n"
+	f.close
+	erb :message
+end
+
+
+get '/login' do
+	erb :login
+end
+post '/' do
+   	@name = params[:username]
+	@pass = params[:password]
+	@message = ""
+	erb :login
+	if @name=="admin"&&@pass=="qwe"
+		erb :welcom
+	else
+		@message="acces denaed"
+		erb :login	
+	end
+end
+
+post '/visit' do
+   	@name1 = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@par = params[:par]
+	@color = params[:color]
+	
+	@title = "Thank you!"
+	@message = "Dear #{@name1}(#{@phone}), we'll be waiting for you at #{@datetime}. Your barber is #{@par}. Ваш цвет: #{@color}"
+   	
+	
+	f = File.open "./public/data.txt", "a"
+	f.write "User: #{@name1}, Phone: #{@phone}, Date and time: #{@datetime}. Par...:#{@par}\n"
+	f.close
+
+        erb :message
+	
+end
+
+
